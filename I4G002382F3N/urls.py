4bc1b291a9eccadf466f603b841d306184617e56
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-06-24 00:11:35 +0100

    Worked on the urls so that it automatically open the blog home when visited

--- I4G002382F3N/urls.py
+++ I4G002382F3N/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls", namespace = "blog"))
+    path("blog/", include("blog.urls", namespace = "blog")),
+    path("", include("blog.urls", namespace="all")),
 ]
